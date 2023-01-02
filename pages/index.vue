4ec55be4880a8f4dad2b746a3b6d2867e0a31f47
LancerAbir <anjan.o24@gmail.com>
2023-01-03 01:08:34 +0600

    asda

--- pages/index.vue
+++ pages/index.vue
@@ -15,4 +15,23 @@
 
         <AppAlert> This is an auto-imported component </AppAlert> <br />
     </div>
+
+    <h4>Data Fatching</h4>
+    <div v-if="pending">Loading ..........</div>
+    <div v-else>
+        <ul>
+            <li v-for="user in users" :key="user.id">
+                {{ user.id }} Title: {{ user.title }}
+            </li>
+        </ul>
+    </div>
 </template>
+
+<script setup>
+// const { data: count } = await useFetch("/api/count");
+const { pending, data: users } = await useFetch(
+    "https://jsonplaceholder.typicode.com/todos/"
+);
+
+console.log(users.data);
+</script>
